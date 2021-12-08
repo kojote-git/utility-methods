cc8469941b38861e55e627c6bce52af4dc123f11
Pavlo Budantsev <pavlo.budantsev@gmail.com>
2021-12-08 19:22:36 +0200

    added methods to implement

--- src/main/java/org/example/utility/UtilityMethods.java
+++ src/main/java/org/example/utility/UtilityMethods.java
@@ -7,6 +7,15 @@ public final class UtilityMethods {
 
     public static int add(int a, int b) {
         return a + b;
+    }
 
+    public static int subtract(int a, int b) {
+        // TODO;
+        return 0;
+    }
+
+    public static int multiply(int a, int b) {
+        // TODO
+        return 0;
     }
 }
