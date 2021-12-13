2aae26b590b56d0202b3ec686d17bbfb4bdf1b73
azelionni@gmail.com <azelionni@gmail.com>
2021-12-13 10:43:01 +0200

    "Test added"

--- src/test/java/org/exmple/utility/UtilityMethodsTest.java
+++ src/test/java/org/exmple/utility/UtilityMethodsTest.java
@@ -104,10 +104,8 @@ public class UtilityMethodsTest {
     @Test
     public void reverse() {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] check = array;
+        int[] check = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
         UtilityMethods.reverse(array);
-        UtilityMethods.reverse(check);
-                Assertions.assertEquals(check, array);
-
+        Assertions.assertArrayEquals(check, array);
     }
 }
